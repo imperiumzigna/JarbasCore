c487146ca27f9f6bb92b12bdad4bb2851bcd8fd8
imperiumzigna <igoramorimsilva2@gmail.com>
2016-04-19 15:46:29 -0300

    angular controllers

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -2,3 +2,33 @@
  * Created by igor on 15/04/16.
  */
 
+var jarbas = angular.module('jarbas')
+
+
+.controller('dashCtrl',["passport"],function($scope){
+  if(!req.isAuthenticated()){
+res.status(403);
+    window.redirect('/403');
+
+  }
+ var me = function ($scope) {
+   return $scope.passport.user.username
+  }
+
+ var searchDevice = function($scope,req,res){
+
+ },
+
+   connectDevice = function(){
+
+ },
+   errorDevice = function(){
+
+  },
+
+   sendComandDevice = function(){
+
+   }
+
+
+})
